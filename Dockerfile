ed2d3d13a4475be0e1368a2d0af921357cf9e9eb
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-15 13:58:36 -0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+
+FROM node:21
+
+# turn off extra external api calls
+ENV NPM_CONFIG_UPDATE_NOTIFIER false
+ENV NPM_CONFIG_FUND false
+
+WORKDIR /app
+
+COPY next.config.js package*.json ./
+
+RUN npm ci --omit=dev
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD [ "npm", "start"]
--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,37 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            // href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+
             <Image
-              src="/vercel.svg"
+              src="/lo_short.svg"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              // className={styles.vercelLogo}
+              width={75}
+              height={75}
               priority
             />
           </a>
         </div>
+        <p>
+          More coming&nbsp;
+          <code className={styles.code}>soon.</code>
+        </p>
       </div>
 
       <div className={styles.center}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
+          // className={styles.logo}
+          src="/lo_long.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={500}
+          height={500}
           priority
         />
       </div>
