cfa860c5a262ebc79fbf6ccb4aed343d0cd20481
Michael Hofweller <michael.hofweller@gmail.com>
2023-12-27 16:56:59 -0800

    page updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Bowlby_One } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+// replace font, use typescript. 
 
 export const metadata: Metadata = {
   title: 'Create Next App',
